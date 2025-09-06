e8a3689a038edaa839bf40e0d23b6dffaf363979
Dex564 <orlovpetr2007@gmail.com>
2025-09-06 12:54:44 +0400

    added admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from .models import Product, Category
 
-@admin.register
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug'] # list display - параметры из models.Category которые будут отображаться в админке
+    prepopulated_fields = {'slug': ('name',)} # это поля, которые будут заполнены автоматически
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'available', 'created', 'updated']
+    list_filter = ['category', 'available', 'updated', 'created'] # как можно фильтровать товары в АДМИНКЕ
+    list_editable = ['price', 'available'] # параметры, которые можно менять
+    prepopulated_fields = {'slug': ('name',)}
