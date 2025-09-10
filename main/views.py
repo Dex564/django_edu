04f1554209812e01433f7c42188c1b4ce28205b7
Dex564 <orlovpetr2007@gmail.com>
2025-09-10 20:03:01 +0400

    added templates, have working link which takes you to main page

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+
+def product_list(request, category_slug=None): # cat_slug это парамеры запроса по фильтрации, в начале никак не надо фильтровать поэтому None
+    categories = Category.objects.all() # достаём все категории из бд
+    products = Product.objects.filter(available=True)
+    
+    category = None
+
+    if category_slug:
+        category = get_object_or_404(Category, category_slug=category_slug) # получаем категории из бд по category_slug
+        products = products.filter(category=category)
+    
+    return render(request, 'main/product/list.html', 
+                {
+                    'category': category,
+                    'categories': categories,
+                    'products': products
+                })
+    
+    
