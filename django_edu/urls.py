99cf723fd88a3ac9c608527266330c8aab7ce580
Dex564 <orlovpetr2007@gmail.com>
2025-09-20 12:00:08 +0400

    added all main templates

--- django_edu/urls.py
+++ django_edu/urls.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')), # namespace - есть в base.html потом понадобится для обращений, чтобы вести редиректы, делать ссылки в шаблонах
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
