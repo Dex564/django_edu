623e86f18076cca1efff86aed07fb37dc861c207
Dex564 <orlovpetr2007@gmail.com>
2025-09-21 20:31:38 +0400

    fixed another problem in cart app

--- cart/cart.py
+++ cart/cart.py
@@ -21,7 +21,6 @@ class Cart:
             self.cart[product_id][quantity] = quantity
         else:
             self.cart[product_id][quantity] += quantity
-
         self.save()
             
     
